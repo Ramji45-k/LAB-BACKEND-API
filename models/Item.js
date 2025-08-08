8218200defff3e8610b61254434def9fe03530b0
Ramji K S <rks051500@gmail.com>
2025-08-08 15:17:39 +0530

    ADDED server.js,models and routers for CURD API

---
+++ models/Item.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const itemSchema = new mongoose.Schema
+({
+  name: {
+    type: String,
+    required: true,
+
+    },
+    quantity: {
+    type: Number,
+    required: true,
+    },}
+);
+module.exports = mongoose.model('Item', itemSchema);
+
